1d66d34be18cfbbbf211130af5a3a26942ee0b81
soniabergamini <sounia96@outlook.it>
2023-05-26 01:29:54 +0200

    feat(main): add movies/series cover

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,6 +18,7 @@ export default {
             } else {
                 return `fi fi-${lang}`
             }
+            // ALTERNATIVE: add "fi fi-eu" where the flag is missing, or a custom icon
         }
     }
 }
@@ -29,10 +30,14 @@ export default {
     <section>
 
         <!-- Movies -->
-        <section v-if="store.AllMovies.length > 0 && !store.errorMsg && !this.store.loading">
-            <!-- <pre class="text-xs">{{ store.AllMovies }}</pre> -->
+        <section class="border border-violet-400"
+            v-if="store.AllMovies.length > 0 && !store.errorMsg && !this.store.loading">
+            <pre class="text-xs">{{ store.AllMovies }}</pre>
             <h2>MOVIES</h2>
             <ol v-for="item in store.AllMovies[0]" class="list-disc px-2 border border-pink-400">
+                <li></li>
+                <img :src="`${store.ImgPath}w92${item.poster_path}`" alt="movie-poster">
+                </li>
                 <li>
                     <span class="text-xs"><strong class="text-sm">Title: </strong>{{ item.title }}</span>
                 </li>
@@ -50,10 +55,13 @@ export default {
         </section>
 
         <!-- TV Series -->
-        <section v-if="store.AllSeries.length > 0 && !store.errorMsg && !this.store.loading">
+        <section class="border border-lime-400" v-if="store.AllSeries.length > 0 && !store.errorMsg && !this.store.loading">
             <!-- <pre class="text-xs">{{ store.AllSeries[0] }}</pre> -->
             <h2>TV SERIES</h2>
             <ol v-for="item in store.AllSeries[0]" class="list-disc px-2 border border-pink-400">
+                <li>
+                    <img :src="`${store.ImgPath}w92${item.poster_path}`" alt="tvserie-poster">
+                </li>
                 <li>
                     <span class="text-xs"><strong class="text-sm">Title: </strong>{{ item.name }}</span>
                 </li>
--- src/data/store.js
+++ src/data/store.js
@@ -7,5 +7,6 @@ export const store = reactive({
     APIMovies: 'https://api.themoviedb.org/3/search/movie?api_key=0bf37c03732d535d0cb540cf8b23e0f1',
     APISeries: 'https://api.themoviedb.org/3/search/tv?api_key=0bf37c03732d535d0cb540cf8b23e0f1',
     AllMovies: [],
-    AllSeries: []
+    AllSeries: [],
+    ImgPath: 'https://image.tmdb.org/t/p/'
 });
